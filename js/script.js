a6342a08c7604210fdba0db57bf8b6e1b1e59bdf
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-27 13:57:26 +0200

    add-data-convert

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // js
 
 
+// base dati
 const posts = [
     {
         "id": 1,
@@ -58,3 +59,62 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+// variabili
+const container = document.getElementById('container');
+
+//converto le date in formato europeo
+convertData();
+
+// aggiungo i post in pagina
+getPosts();
+
+
+
+
+
+// funzioni
+function getPosts(){
+    let content = '';
+    posts.forEach(post => {
+        content += `<div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${post.author.image}" alt="${post.author}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${post.author.name}</div>
+                    <div class="post-meta__time">${post.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+        <div class="post__image">
+            <img src="${post.media}" alt="${post.image}">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+    })
+
+    container.innerHTML = content;
+}
+
+function convertData(){
+    posts.forEach(post => {
+        post.created = post.created.split("-").reverse().join("-");
+    })
+}
